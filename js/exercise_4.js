ee14db37ca303399b2eb5419071259d6041dee7b
Kamineri <kamineri.lotro@gmail.com>
2016-04-25 10:08:16 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -40,7 +40,7 @@ featureLayer.on('ready', function(){
 //add popups
 
 featureLayer.on('ready', function(){
-  this.eachLayer(function(Layer){
+  this.eachLayer(function(layer){
   	layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
   })
 })
