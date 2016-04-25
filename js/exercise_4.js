c21e7f0733130ba75ea1c0cd4d1d8ed86423dbd7
Kamineri <kamineri.lotro@gmail.com>
2016-04-25 10:43:31 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -38,10 +38,43 @@ featureLayer.on('ready', function(){
 })
 
 //add popups
+//featureLayer.on('ready', function(){
+//  this.eachLayer(function(layer){
+//  	layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
+//  })
+//})
+
+// instead of popup, use info window to display attributes
+
+var clickHandler = function(e){
+
+  $('#info').empty();
+  
+  var feature = e.target.feature;
+  
+  $('#info").fadeIn(400, function(){
+    var info = ' ';
+    info += '<div>',
+    info += 	'<h2>' + feature.properties.LABEL + '<h2>';
+    info += 	'<p>' + feature.properties.LACATION + '<p>';
+    info += '<div>';
+    
+    $('#info').append(info);
+    
+    })
+  
+}
 
 featureLayer.on('ready', function(){
-  this.eachLayer(function(layer){
-  	layer.bindPopup('Welcome to ' + layer.feature.properties.LABEL);
-  })
+	this.eachLayer(function(layer){
+      layer.on('click', clickHandler);
+      
+    })
+
 })
 
+map.on('click', function(){
+	$('#info').fadeOut(200);
+  	$('#info').empty();
+
+})
