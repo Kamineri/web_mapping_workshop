e8befae58c1fd729489aa8d1ac5f1d374cba1da1
Kamineri <kamineri.lotro@gmail.com>
2016-04-25 08:51:49 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'lbarnhill.pp97efpm';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1IjoibGJhcm5oaWxsIiwiYSI6ImNpbmcwdDhoNTE5ejN2MGtxbDdnbnR1MGIifQ.74dEiHuZO7_19HLDZCecOw';
 
 // Create the map object with your mapId and token, 
 // referencing the DOM element where you want the map to go.
